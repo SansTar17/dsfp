931f9fe3c6993112f0d2b1055b95268828096001
Muhammad Amirul Hafiz Bin Osman <125260105+SansTar17@users.noreply.github.com>
2023-05-30 20:45:06 +0800

    Create Participant.java

---
+++ PROJECT_01/Participant.java
@@ -0,0 +1,16 @@
+package PROJECT_01;
+
+// Participant class as a subclass
+public class Participant {
+
+    private String name;
+
+    public Participant(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+}
