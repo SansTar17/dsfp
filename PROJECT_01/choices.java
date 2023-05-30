ad1fb9f3f97be7bd7f08c0e882a89737100a0b1d
Muhammad Amirul Hafiz Bin Osman <125260105+SansTar17@users.noreply.github.com>
2023-05-30 15:37:55 +0800

    Create choices.java

---
+++ PROJECT_01/choices.java
@@ -0,0 +1,15 @@
+package PROJECT_01;
+
+public class choices {
+
+    //Register training and add participant
+    public void choice_1(){
+
+
+    }
+    //Sorting and display participant list
+    public void choice_2(){
+
+
+    }
+}
