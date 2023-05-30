5f0860434df598e3da4f73e9f0bff96f183d0cda
Muhammad Amirul Hafiz Bin Osman <125260105+SansTar17@users.noreply.github.com>
2023-05-30 20:42:19 +0800

    Update choices.java

--- PROJECT_01/choices.java
+++ PROJECT_01/choices.java
@@ -1,15 +1,37 @@
 package PROJECT_01;
 
-public class choices {
+import java.util.Scanner;
 
+public class Choices {
+
+    // Declare the LinkedList field
+    private LinkedList<Participant> participantList = new LinkedList<>();    
+    //Declare the global scanner
+    private Scanner sc = new Scanner(System.in);        
+  
     //Register training and add participant
     public void choice_1(){
+        
+        System.out.println("Please enter your name:");
 
+        String name = sc.nextLine();
 
+        // Create a new Participant object
+        Participant participant = new Participant(name);
+        
+        participantList.addLast(participant);
+        
     }
+
     //Sorting and display participant list
     public void choice_2(){
 
-
-    }
+        MergeSort.mergeSort(participantList);
+    
+        // Display the sorted participant list
+        for (int i = 0; i < participantList.size(); i++) {
+            Participant participant = participantList.get(i);
+            System.out.println("hi "+participant.getName());
+        }        
+    }    
 }
