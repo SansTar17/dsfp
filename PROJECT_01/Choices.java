af41bdc2389f4383309dee63a309d76d721eb935
Muhammad Amirul Hafiz Bin Osman <125260105+SansTar17@users.noreply.github.com>
2023-05-31 21:20:53 +0800

    Update Choices.java
    update 1.0

--- PROJECT_01/Choices.java
+++ PROJECT_01/Choices.java
@@ -7,17 +7,21 @@ public class Choices {
     // Declare the LinkedList field
     private LinkedList<Participant> participantList = new LinkedList<>();    
     //Declare the global scanner
-    private Scanner sc = new Scanner(System.in);        
+    private Scanner scanner = new Scanner(System.in);        
   
     //Register training and add participant
     public void choice_1(){
         
         System.out.println("Please enter your name:");
 
-        String name = sc.nextLine();
+        String name = scanner.nextLine();
+
+        //add code to assign id
+        Integer Id = 01; //ChangeLater
+        
 
         // Create a new Participant object
-        Participant participant = new Participant(name);
+        Participant participant = new Participant(name,Id);
         
         participantList.addLast(participant);
         
@@ -31,21 +35,23 @@ public class Choices {
         // Display the sorted participant list
         for (int i = 0; i < participantList.size(); i++) {
             Participant participant = participantList.get(i);
-            System.out.println(participant.getName());
+            System.out.println("hi "+participant.getName());
         }        
     }
 
     public void choice_3() {
+        
        System.out.println("Enter participant name to search:");
-                    String searchName = scanner.nextLine();
-                    Node<Participant> searchResult = LinkedList.searchParticipant(searchName);
-                    if (searchResult != null) {
-                        System.out.println("Participant found!");
-                        System.out.println("Name: " + searchResult.data.getName() + ", ID: " + searchResult.data.getId());
-                    } else {
-                        System.out.println("Participant not found.");
-                    }
-                    break;
+       String searchName = scanner.nextLine();
+
+       Participant searchResult = SearchingAlgorithms.searchParticipant(searchName, participantList);
+
+       if (searchResult != null) {
+           System.out.println("Participant found!");
+           System.out.println("Name: " + searchResult.getName() + ", ID: " + searchResult.getId());
+       } else {
+           System.out.println("Participant not found.");
+       }
+       
     }
-    }    
-}
+}    
