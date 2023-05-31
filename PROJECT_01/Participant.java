0e70225dd23196e35a302264b049b6b7fda7647c
Muhammad Amirul Hafiz Bin Osman <125260105+SansTar17@users.noreply.github.com>
2023-05-31 21:22:10 +0800

    Update Participant.java

--- PROJECT_01/Participant.java
+++ PROJECT_01/Participant.java
@@ -3,15 +3,20 @@ package PROJECT_01;
 // Participant class as a subclass
 public class Participant {
 
-    //can add more things here
     private String name;
+    private Integer Id;
 
-    public Participant(String name) {
+    public Participant(String name,Integer Id) {
         this.name = name;
+        this.Id = Id;
+        //training_course
     }
 
     public String getName() {
         return name;
     }
     
+    public Integer getId() {
+        return Id;
+    }
 }
