2d62354216a2d04e9a1ecd998d76d8aadf64e88a
nicholas0802 <135084275+nicholas0802@users.noreply.github.com>
2023-06-03 22:11:46 +0800

    Update Choices.java

--- PROJECT_01/Choices.java
+++ PROJECT_01/Choices.java
@@ -2,30 +2,82 @@ package PROJECT_01;
 
 import java.util.Scanner;
 
-public class Choices {
+public class Choices{
 
     // Declare the LinkedList field
     private LinkedList<Participant> participantList = new LinkedList<>();    
     //Declare the global scanner
-    private Scanner scanner = new Scanner(System.in);        
+    private Scanner scanner = new Scanner(System.in); 
+    
+    private boolean isParticipantRegistered(String name) {
+        for (int i = 0; i < participantList.size(); i++) {
+            Participant participant = participantList.get(i);
+            if (participant.getName().equalsIgnoreCase(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isIdRegistered(int id) {
+        for (int i = 0; i < participantList.size(); i++) {
+            Participant participant = participantList.get(i);
+            if (participant.getId().equals(id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+       
   
     //Register training and add participant
-    public void choice_1(){
-        
-        System.out.println("Please enter your name:");
+    public boolean choice_1(){
 
+        
+        
+        System.out.println("Please enter your name (or 'stop' to finish):");
+
+       while (true) {
+        
+        System.out.println("Your name:");
         String name = scanner.nextLine();
 
-        //add code to assign id
-        Integer Id = 01; //ChangeLater
+       
+        if (isParticipantRegistered(name)) {
+            System.out.println("Participant already registered!");
+            continue; // Continue to the next iteration of the loop
+        }//request to type again if name registered
+
+                    
+        if (name.equalsIgnoreCase("stop")) {
+            break; // Exit the loop if the user enters 'stop'
         
+        }
+        
+        System.out.println("Your id:");
+        int id = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character after reading the integer
+
+        if (isIdRegistered(id)) {
+            System.out.println("ID already registered!");
+            continue;
+        }//request to type again if id registered
+             
 
         // Create a new Participant object
-        Participant participant = new Participant(name,Id);
+        Participant participant = new Participant(name,id);
         
         participantList.addLast(participant);
-        
+
     }
+    return false;
+                
+        
+}
+
+
+    
 
     //Sorting and display participant list
     public void choice_2(){
@@ -35,7 +87,7 @@ public class Choices {
         // Display the sorted participant list
         for (int i = 0; i < participantList.size(); i++) {
             Participant participant = participantList.get(i);
-            System.out.println("hi "+participant.getName());
+            System.out.println("hi "+participant.getName()+" "+"id:"+participant.getId());
         }        
     }
 
@@ -48,7 +100,7 @@ public class Choices {
 
        if (searchResult != null) {
            System.out.println("Participant found!");
-           System.out.println("Name: " + searchResult.getName() + ", ID: " + searchResult.getId());
+           System.out.println("Name: " + searchResult.getName() + " "+"ID:" + searchResult.getId());
        } else {
            System.out.println("Participant not found.");
        }
