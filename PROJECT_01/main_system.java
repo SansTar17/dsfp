a9384e07c3fcb6a3fba712929aaa6c5438d824a0
Muhammad Amirul Hafiz Bin Osman <125260105+SansTar17@users.noreply.github.com>
2023-06-08 23:29:02 +0800

    Update main_system.java

--- PROJECT_01/main_system.java
+++ PROJECT_01/main_system.java
@@ -1,51 +1,62 @@
 package PROJECT_01;
 
-import java.util.Scanner;
+public class MergeSort {
 
-public class main_system{
-    public static void main(String[] args){
-
-        Scanner sc = new Scanner(System.in);
+    public static void mergeSort(LinkedList<Participant> list) {
         
-        boolean conds = true;
-        Choices choices = new Choices();
-
-        while (conds) {
-            System.out.println("\nENTER YOUR OPTION:");
-            System.out.println("[1]: \tRegister training and add participant");
-            System.out.println("[2]: \tSorting and display participant list");
-            System.out.println("[3]: \tSearch participant");
-            System.out.println("[4]: \tExit Program");
-            
-            int choice = sc.nextInt();
-            sc.nextLine(); 
-
-            switch (choice) {
-
-                case 1:
-                    //code start here
-                    choices.choice_1();
-                    break;
-                case 2:
-                    //code start here
-                    choices.choice_2();
-                    break;
-                case 3:
-                    //code start here
-                    choices.choice_3();
-                    break;
-                case 4:
-                    // exit program
-                    System.out.println("Exiting program...");
-                    conds = false;
-                    break;
-                default:
-                    System.out.println("Invalid input. Please enter a number between 1 and 4.");
-            }
-            
+        // Base case: If the list has one element or is empty, it is already sorted
+        if (list.size() <= 1) {
+            return;
         }
-        
-        sc.close(); // close scanner object
+
+        // Divide the list into two halves
+        LinkedList<Participant> leftHalf = new LinkedList<>();
+        LinkedList<Participant> rightHalf = new LinkedList<>();
+        int middle = list.size() / 2;
+        for (int i = 0; i < middle; i++) {
+            leftHalf.addLast(list.get(i));
+        }
+        for (int i = middle; i < list.size(); i++) {
+            rightHalf.addLast(list.get(i));
+        }
+
+        // Recursively sort the left and right halves
+        mergeSort(leftHalf);
+        mergeSort(rightHalf);
+
+        // Merge the sorted halves back together
+        merge(leftHalf, rightHalf, list);
+    }
+
+    private static void merge(LinkedList<Participant> left, LinkedList<Participant> right, LinkedList<Participant> result) {
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int resultIndex = 0;
+
+        // Compare and merge elements from left and right halves into the result list
+        while (leftIndex < left.size() && rightIndex < right.size()) {
+            Participant leftParticipant = left.get(leftIndex);
+            Participant rightParticipant = right.get(rightIndex);
+
+            // Compare names and add the smaller one to the result list
+            if (leftParticipant.getName().compareTo(rightParticipant.getName()) <= 0) {
+                result.set(resultIndex++, leftParticipant);
+                leftIndex++;
+            } else {
+                result.set(resultIndex++, rightParticipant);
+                rightIndex++;
+            }
+        }
+
+        // Add any remaining elements from the left half
+        while (leftIndex < left.size()) {
+            result.set(resultIndex++, left.get(leftIndex++));
+        }
+
+        // Add any remaining elements from the right half
+        while (rightIndex < right.size()) {
+            result.set(resultIndex++, right.get(rightIndex++));
+        }
     }
     
 }
